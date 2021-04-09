97dabd526d02645c725fb3f3e8e3f319cef91951
dev1 <devl1@mail.com>
2021-04-09 23:05:43 +0530

    hello from dev2

--- hello.java
+++ hello.java
@@ -1,10 +1,10 @@
-class hello()
+class hello()    #from dev2
 {
-	public static void main()
-	{
-		public static void main("hello")
+	public static void main()  #from dev2
+	{  
+	  	public static void main("hello from dev2")
 	}
 
 
-}
+}  #from dev2
 
